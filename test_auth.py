ad7420a51b3d7e70b023e1783e75b0d0a93f43a0
Soulsaw <ssawadogo292@gmail.com>
2024-05-07 21:09:38 +0000

    flas=ke8 requirements

--- test_auth.py
+++ test_auth.py
@@ -1,6 +1,5 @@
 import unittest
 from models.user import User
-from auth import Auth
 """Importation des modules"""
 
 
@@ -17,15 +16,3 @@ class TestAuth(unittest.TestCase):
         user_sav = User().get(user.username)
         self.assertIsNotNone(user_sav)
         self.assertEqual(user_sav.username, user.username)
-
-    def test_login(self):
-        """Tester l'authentification"""
-        user = self.user
-"""         log_user = Auth().login(user.username, user.password)
-        self.assertIsInstance(log_user, User)
-        self.assertIsNotNone(log_user)
-        self.assertEqual(log_user.username, user.username)
-        wrong_pass = Auth().login(user.username, "wrong_password")
-        self.assertIsNone(wrong_pass)
-        wrong_uname = Auth().login("no_exists", user.password)
-        self.assertIsNone(wrong_uname) """
